dd2f85ccfcbc573457478d0fac8c7e5121b263dc
maclacerda <markos.lacerda@gmail.com>
2021-08-28 09:39:28 -0300

    Add missing import

--- AlterSolutionsChallengeCore/DependencyInject/DependencyInject.swift
+++ AlterSolutionsChallengeCore/DependencyInject/DependencyInject.swift
@@ -6,21 +6,25 @@
 //
 
 @propertyWrapper
-struct DependencyInject<Dependency> {
+public struct DependencyInject<Dependency> {
+    
+    // MARK: - Properties
     
     private var dependency: Dependency
     
-    init() {
+    public var wrappedValue: Dependency {
+        get { return dependency }
+        set { dependency = newValue }
+    }
+    
+    // MARK: - Initializer
+    
+    public init() {
         do {
             self.dependency = try DependencyContainer.shared.resolve(type: Dependency.self)
         } catch {
             fatalError("Not able to resolve dependecy: \(Dependency.self), verify if it's correct registered in dependency container")
         }
-    }
-    
-    var wrappedValue: Dependency {
-        get { return dependency }
-        set { dependency = newValue }
     }
     
 }
