6f76cde67a0e28c6c238681b6097d739cdda511d
Marcos Lacerda <4759987+maclacerda@users.noreply.github.com>
2021-08-27 21:49:42 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AlterSolutionsChallengeCore",
-            url: "https://github.com/maclacerda/alter-solutions-challenge-core/raw/master/binary-framework/alter-solutions-challenge-core.xcframework.zip",
+            url: "https://github.com/maclacerda/alter-solutions-challenge-core/raw/main/binary-framework/alter-solutions-challenge-core.xcframework.zip",
             checksum: "18f48537e5d534dc19960d3bdd48297f6c069950a11694d22cbc9bb182be412e"
         )
     ]
